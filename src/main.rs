05cebcf6fac9f04db451132b1ab014c60bce8978
Thomas Heartman <thomas.heartman+git@gmail.com>
2020-12-28 12:29:57 +0100

    Add comment and troublesome line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // trying to expand the below line seems to succeed or fail based
+    // on recent actions in the current buffer. To expand it, move the
+    // cursor to the end of the line and use Rust Analyzer's
+    // post-completion function for `expr.if`.
+    true.if
+
     println!("Hello, world!");
 }
